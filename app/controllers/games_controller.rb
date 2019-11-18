ea2673c8ce057c54869c47ad0dee5440cb1c4fef
christy michel <christy.michel@sfr.fr>
2019-11-18 20:53:27 +0100

    encours

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,9 @@
+class GamesController < ApplicationController
+  def new
+    @letters = (0...10).map { ('A'..'Z').to_a[rand(26)] }.join(' ')
+  end
+
+  def score
+    letters = params[:letters]
+  end
+end
